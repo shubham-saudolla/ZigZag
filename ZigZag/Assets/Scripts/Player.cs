40f9e37953a5f1796c8de0815ec15e96c2014aaa
shubham-saudolla <shubhamsaudolla@gmail.com>
2019-03-20 16:30:37 +0530

    Set up basic player movement

---
+++ ZigZag/Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 9.0f;
+
+    public Vector3 dir;
+
+    private void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (dir == Vector3.forward)
+            {
+                dir = Vector3.left;
+            }
+            else
+            {
+                dir = Vector3.forward;
+            }
+        }
+
+        transform.Translate(dir * _speed * Time.deltaTime);
+    }
+}
