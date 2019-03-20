a24b41da0d9bf4629b755dc8ca1a97cefef008e5
shubham-saudolla <shubhamsaudolla@gmail.com>
2019-03-20 18:00:39 +0530

    Added the triggers and tile script to the tiles

---
+++ ZigZag/Assets/Scripts/Tile.cs
@@ -0,0 +1,21 @@
+﻿/*
+Copyright (c) Shubham Saudolla
+https://github.com/shubham-saudolla
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
