b01ad1c91ed67408098b8e345f5d1d01a1cb4454
shubham-saudolla <shubhamsaudolla@gmail.com>
2019-03-21 17:08:32 +0530

    Decreased the time for the tiles to fall

--- ZigZag/Assets/Scripts/StartTile.cs
+++ ZigZag/Assets/Scripts/StartTile.cs
@@ -26,7 +26,7 @@ public class StartTile : MonoBehaviour
 
     private IEnumerator Fall()
     {
-        yield return new WaitForSeconds(0.3f);
+        yield return new WaitForSeconds(0.1f);
         _rb.isKinematic = false;
 
         yield return new WaitForSeconds(3);
--- ZigZag/Assets/Scripts/Tile.cs
+++ ZigZag/Assets/Scripts/Tile.cs
@@ -27,7 +27,7 @@ public class Tile : MonoBehaviour
 
     private IEnumerator FallAndReplace()
     {
-        yield return new WaitForSeconds(0.3f);
+        yield return new WaitForSeconds(0.1f);
         _rb.isKinematic = false;
 
         yield return new WaitForSeconds(1);
